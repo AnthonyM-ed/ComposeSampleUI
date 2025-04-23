4ed444cebbde9727c0bf65dff27e2879c5f60063
AnthonyM <tonymiyo132@gmail.com>
2025-04-23 08:38:30 -0500

    Create project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.composesampleui"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.composesampleui"
